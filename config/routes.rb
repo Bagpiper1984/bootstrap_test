c18d6a91979d19d25361462e8a2baada1a8984ef
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-14 00:03:44 +0400

    Added Gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ TestWinBootstrap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'session#new'
 
   # See how all your routes lay out with "rake routes"
 
