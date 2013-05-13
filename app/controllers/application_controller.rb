787217db8780fbe65df192465e48c8414efba034
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-13 21:04:23 +0400

    Authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
+  
   protect_from_forgery
+  
+  protected
+  
+    def authorize
+      if User.count != 0
+	    unless User.find_by_id(session[:user_id])
+	    redirect_to login_url, notice: "Please, authorize"
+	  end	
+    end
+  end	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
+  after_destroy :ensure_an_admin_remains
+  
   attr_accessible :email, :name, :password_digest, :password, :password_confirmation
   
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
+  
+  private
+	def ensure_an_admin_remains
+	  if User.count.zero?
+		raise "Last user can't be delete"
+	  end
+	end
 end
