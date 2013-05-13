403fd2f12a6cae7c792e63a2cc93e43ddf85bf4e
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-13 07:31:15 +0400

    Changing User Resourses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password_digest, :password, :password_confirmation
+  
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
